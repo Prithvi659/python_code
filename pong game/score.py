e54962ab7dcf7cd53e56dbe4c54455a3d81f79fd
Prithvi659 <145311474+Prithvi659@users.noreply.github.com>
2025-12-04 19:05:00 +0530

    developed pong with python using turtle module

---
+++ pong game/score.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        # Left player score
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        # Right player score
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def game_over(self, winner: str = ""):
+        self.goto(0, 0)
+        message = "Game Over"
+        if winner:
+            message += f" - {winner} wins"
+        self.write(message, align="center", font=("Courier", 30, "bold"))
